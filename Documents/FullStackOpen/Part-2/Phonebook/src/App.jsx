13ece4abde99e67164e8f49bc168f8b0502dd87c
PriceLlewellyn <price13.llewellyn@gmail.com>
2026-04-12 10:27:31 +0100

    Exercise 2.9: event handler updated, name and number display side by side

--- Documents/FullStackOpen/Part-2/Phonebook/src/App.jsx
+++ Documents/FullStackOpen/Part-2/Phonebook/src/App.jsx
@@ -2,13 +2,9 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567' }
   ]) 
   const [newName, setNewName] = useState('')
-
-  const [phoneNumber, setPhoneNumber] = useState([
-    {number: '040-1234567'}
-  ])
   const [newNumber, setNewNumber] = useState('')
 
 
@@ -29,15 +25,16 @@ const addPerson = (event) => {
     number: newNumber
   }
     setPersons(persons.concat(nameObject))
-    setPhoneNumber(phoneNumber.concat(nameObject))
     setNewName('')
     setNewNumber('')
   }
 }
 
-const handlePersonUpdate = (event) => {
-  console.log(event.target.value)
+const handleNameUpdate = (event) => {
   setNewName(event.target.value)
+}
+
+const handleNumberUpdate = (event) => {
   setNewNumber(event.target.value)
 }
 
@@ -48,13 +45,13 @@ const handlePersonUpdate = (event) => {
         <div>
           name: <input 
           value={newName}
-          onChange={handlePersonUpdate}
+          onChange={handleNameUpdate}
           />
         </div>
         <div>
           number: <input 
           value={newNumber}
-          onChange={handlePersonUpdate}/>
+          onChange={handleNumberUpdate}/>
         </div>
         <div>
           <button type="submit" >add</button>
@@ -63,10 +60,7 @@ const handlePersonUpdate = (event) => {
       <h2>Numbers</h2>
       <div>
         {persons.map(person => 
-          <li key={person.name}>{person.name}</li>
-        )}
-        {phoneNumber.map(phoneNumber => 
-          <li key={phoneNumber.number}>{phoneNumber.number}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         )}
       </div>
     </div>
